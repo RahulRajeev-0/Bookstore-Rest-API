d472227b07f4b804012df455ae34c76c3e8e2db3
Rahul Rajeev <rahulrajeev836@gmail.com>
2025-02-08 16:16:44 +0530

    start work on books

---
+++ bookstore/books/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    biography = models.TextField()
+    date_of_birth = models.DateField()
+    nationality = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    isbn = models.CharField(max_length=13, unique=True) # length of isbn no is 13 
+    publication_date = models.DateField()
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    genre = models.CharField(max_length=100)
+    description = models.TextField()
+    publisher = models.CharField(max_length=100)
+    authors = models.ManyToManyField(Author) # many to many = sometimes (multiple author write a book and book have multiple authors)
+
+    def __str__(self):
+        return self.title
+    
+
+
+
+    
+
